6212c81b321cfd14c2b69bb7c515d8814361640d
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-04-13 21:44:52 +0000

    Main

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# ICD2O Unit #X-YY
+# ICD2O Unit #3-04
 
 [![Mr Coxall's Super Linter](README.md/../../../workflows/Super%20Linter/badge.svg)](README.md/../../../actions)
 
 [![Deploy Pages](README.md/../../../workflows/Deploy%20Pages/badge.svg)](README.md/../../../actions)
 
-This site can be found at: [https://<OWNER>.github.io/<REPOSITORY>](https://<OWNER>.github.io/<REPOSITORY>)
+This site can be found at: 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+"use strict";
+// Script for celsius of a Sphere
+
+// Elements
+// Inputs
+const fdInput = document.getElementById('fdInput');
+// Result Display
+const celsiusResult = document.getElementById('celsiusResult');
+// Calculate Button
+const calculateBtn = document.getElementById('CalculateBtn');
+
+// Code for converting Fahrenheit to Celsius
+function Calculate() {
+
+  // Get fahrenheit degrees Input
+  let fd = Number(fdInput.value);
+
+  // Calculate Celsius
+  let celsius = (fd - 32) * (5/9)
+
+  // Round up to 2 decimal places, if needed
+  if (celsius % 1 != 0) {
+    celsius = celsius.toFixed(2);
+  }
+
+  // Display Result
+  celsiusResult.innerHTML = `${celsius} <sup>o</sup>C`;
+  return;
+}
+
+// Connect button click to the Calculate() function, 
+calculateBtn.onclick = Calculate;
+
+// Initial Calculate() call
+Calculate();
